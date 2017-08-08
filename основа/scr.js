e6e1eec741c32c5e9ebf5cb5322e84994bdba05c
Aris12122 <30840310+Aris12122@users.noreply.github.com>
2017-08-08 21:25:01 +0300

    kek

---
+++ основа/scr.js
@@ -0,0 +1,19 @@
+$('document').ready(function($) {
+//	$('input').on('keyup',function() {
+//		var value1,value2,value3;
+	//	value1=$('#v1').val();
+	//	value2=$('#v2').val();
+	//	value1=parseInt(value1);
+	//	value2=parseInt(value2);
+	//	value3=value1*value2;
+	//	$('#res').html(value3);
+	var test1;
+	//test1=prompt('Write your name','');
+	//$('#res').html(test1);
+	var a1=431;
+	if (a1==432) {
+		alert('Ok!');
+	}else{
+		alert('bad!');
+	}
+	} )
